87568dec457ed46e742472e6c225a59b121d6844
Ubuntu <ubuntu@ip-172-31-58-252.ec2.internal>
2023-10-30 11:28:25 +0000

    Added New Line

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,8 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World");
+
+	System.out.println("This is my new line"); 
     } 
 }
